72f95018e9ac57b2c24ab5437e6c766abd02026a
Sivaraman R <sivaram311@gmail.com>
2019-02-22 19:17:45 +0530

    Intendation Fixed at Desktop.py

--- cva_apps/config/desktop.py
+++ cva_apps/config/desktop.py
@@ -1,11 +1,11 @@
 from frappe import _
 
 def get_data():
-	# return {
-	# 	"Library Management": {
-	# 		"color": "#589494",
-	# 		"icon": "icon-book",
-	# 		"type": "module",
-	# 		"label": _("Library Management")
-	# 	}
-	# }
+# return {
+# 	"Library Management": {
+# 		"color": "#589494",
+# 		"icon": "icon-book",
+# 		"type": "module",
+# 		"label": _("Library Management")
+# 	}
+# }
